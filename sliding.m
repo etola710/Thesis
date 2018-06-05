57a51585243e7ca136a74f3cfc643951622434b1
etola710 <eric.tola@hotmail.com>
2018-06-04 20:13:35 -0700

    updates for j1

--- sliding.m
+++ sliding.m
@@ -2,32 +2,34 @@ clearvars
 close all
 %Sliding Motion
 addpath(genpath('instantaneous_mechanism_method'))
+mp = struct();
 %finger dimensions
-links = [.08 .05]; %m
-mass = [.5 .3 .05]; %kg
-I = @(m,l) (m*l^2)/12;
-inertia = [I(mass(1),links(1)) I(mass(2),links(2))]; %moment of inertias kg m^2
+mp.links = [.08 .05]; %m
+mp.mass = [.5 .3 .05]; %kg
+mp.I_rod = @(m,l) (m*l^2)/12;
+mp.I = [mp.I_rod(mp.mass(1),mp.links(1)) mp.I_rod(mp.mass(2),mp.links(2))]; %moment of inertias kg m^2
 %object dimensions
-dim = [.03 .05]; %m [height length]
-dt = .01;
-mu = [.1 .9]; %friction coefficents [object/floor , finger/object]
+mp.dim = [.03 .05]; %m [height length]
+mp.p_con = .03; %contact point x
+mp.dt = .01;
+mp.mu = [.1 .9]; %friction coefficents [object/floor , finger/object]
 %gravity parameters
-g_acc = 9.81;
-g_dir = 3*pi/2;
-g_force = g_acc*sin(g_dir);
-time = [.3 .2 .2 .3]; %s time for each step
-pos = [.1 .05 .1 .05]; %m x coordinate [inital, ..., final]
+mp.g_acc = 9.80665;
+mp.g_dir = 3*pi/2;
+mp.g_force = [mp.g_acc*cos(mp.g_dir) mp.g_acc*sin(mp.g_dir)]; %Fg_x Fg_y
+%motion primitive
+mp.time = [.5 .5 .5]; %s time for each step
+mp.pos = [.05 .1 .05 .1]; %m x coordinate [inital, ..., final]
 %generate sliding motion plan
-[p_j,accel,R,alpha,svaj_curves,tp,xbox,ybox]=sliding_motion(links,pos,dim,time,dt);
-svaj_plot(tp,svaj_curves);
+mp = sliding_motion(mp);
+svaj_plot(mp);
 %lp dynamics
-x=cell(1,length(svaj_curves));
-fval=1:length(svaj_curves);
-for i=1:length(svaj_curves)
-[lp,fval,exitflag] = lp_dynamics_sliding(mass,accel(:,i),alpha(:,i),R(:,i),mu,svaj_curves(2,i),svaj_curves(3,i),inertia,g_force);
-x{i} = lp;
-exitflag
-end
-torque_plot(tp,x);
-filename ='sliding.gif';
-sliding_plot(p_j(1:2,:),p_j(3:4,:),xbox,ybox,length(x),filename,x);
+mp.x=cell(1,length(mp.svaj_curve));
+mp.fval=1:length(mp.svaj_curve);
+mp = lp_dynamics_sliding(mp);
+mp.lp = cell2mat(mp.x);
+mp = torque_plot2(mp);
+mp.filename ='sliding.gif';
+mp.gif_fps=10;
+sliding_plot2(mp);
+
